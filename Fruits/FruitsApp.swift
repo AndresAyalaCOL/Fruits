1479d4496c2a595355434503724580cfa5e2cf32
AndresAyalaCOL <andres.ayala@reorg.com>
2024-10-25 06:33:34 -0500

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Andres Ayala on 25/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
